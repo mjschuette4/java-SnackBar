48ddfa5f23b07ed5de6ce08d2166d5f31e09fc89
MatthewSchuette <mjschuette4@gmail.com>
2020-06-01 17:53:56 -0500

    Finished 3 processing

--- README.md
+++ README.md
@@ -174,15 +174,15 @@ Create the classes mentioned above and then instantiate these objects from those
 
 ### Processing
 
-* [ ] Customer 1 (Jane) buys 3 of snack 4 (Soda).
-  * [ ] Print Customer 1 (Jane) Cash on hand.
-  * [ ] Print quantity of snack 4 (Soda).
-* [ ] Customer 1 (Jane) buys 1 of snack 3 (Pretzel).
-  * [ ] Print Customer 1 (Jane) Cash on hand.
-  * [ ] Print quantity of snack 3 (Pretzel).
-* [ ] Customer 2 (Bob) buys 2 of snack 4 (Soda).
-  * [ ] Print Customer 2 (Bob) Cash on Hand.
-  * [ ] Print quantity of snack 4 (Soda).
+* [x] Customer 1 (Jane) buys 3 of snack 4 (Soda).
+  * [x] Print Customer 1 (Jane) Cash on hand.
+  * [x] Print quantity of snack 4 (Soda).
+* [x] Customer 1 (Jane) buys 1 of snack 3 (Pretzel).
+  * [x] Print Customer 1 (Jane) Cash on hand.
+  * [x] Print quantity of snack 3 (Pretzel).
+* [x] Customer 2 (Bob) buys 2 of snack 4 (Soda).
+  * [x] Print Customer 2 (Bob) Cash on Hand.
+  * [x] Print quantity of snack 4 (Soda).
 * [ ] Customer 1 (Jane) finds $10.
   * [ ] Print Customer 1 (Jane) Cash on Hand.
 * [ ] Customer 1 (Jane) buys 1 of snack 2 (Chocolate Bar).
--- src/snackBar/Main.java
+++ src/snackBar/Main.java
@@ -15,6 +15,29 @@ public class Main {
         Snack s3 = new Snack("Pretzel", 36, 1.75, v1.getId());
         Snack s4 = new Snack("Soda", 36, 1.75, v2.getId());
         Snack s5 = new Snack("Water", 36, 1.75, v2.getId());
+
+        System.out.println("********************************************************************************");
+        System.out.println("********************************************************************************");
+   
+        
+        //Customer 1 buys 3 of snack 4. Print Customer 1 Cash on hand. Print quantity of snack 4.
+        c1.moneySubtract(s4.getCost() * 3);
+        s4.snacksReduce(3);
+        System.out.println(c1.getName() + " has " + "$" + c1.getCash() + " left. " + "There are " + s4.getQuantity() + " " + s4.getName() + "s left.");
+   
+        //Customer 1 buys 1 of snack 3. Print Customer 1 Cash on hand. Print quantity of snack 3.
+        c1.moneySubtract(s3.getCost() * 1);
+        s3.snacksReduce(1);
+        System.out.println(c1.getName() + " has " + "$" + c1.getCash() + " left. " + "There are " + s3.getQuantity() + " " + s3.getName() + "s left.");
+    
+        //Customer 2 buys 2 of snack 4. Print Customer 2 Cash on Hand. Print quantity of snack 4 .
+        c2.moneySubtract(s4.getCost() * 2);
+        s4.snacksReduce(2);
+        System.out.println(c2.getName() + " has " + "$" + c2.getCash() + " left. " + "There are " + s4.getQuantity() + " " + s4.getName() + "s left.");
+    
+        
+    
+    
     }
 
     public static void main(String[] args) {
