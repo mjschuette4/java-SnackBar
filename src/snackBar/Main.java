0fe8978065f93df7261cd52221aebd7c01886f42
MatthewSchuette <mjschuette4@gmail.com>
2020-06-01 18:08:49 -0500

    Finished MVP

--- src/snackBar/Main.java
+++ src/snackBar/Main.java
@@ -35,9 +35,26 @@ public class Main {
         s4.snacksReduce(2);
         System.out.println(c2.getName() + " has " + "$" + c2.getCash() + " left. " + "There are " + s4.getQuantity() + " " + s4.getName() + "s left.");
     
-        
+        //Customer 1 finds $10. Print Customer 1 Cash on Hand.
+        c1.moneyAdd(10);
+        System.out.println(c1.getName() + " found $10, and now has $" + c1.getCash() + " left.");
     
+        //Customer 1 buys 1 of snack 2. Print Customer 1 Cash on Hand. Print quantity of snack 2.
+        c1.moneySubtract(s2.getCost() * 1);
+        s2.snacksReduce(1);
+        System.out.println(c1.getName() + " has " + "$" + c1.getCash() + " left. " + "There are " + s2.getQuantity() + " " + s2.getName() + "s left.");
     
+        //Add 12 more items to snack 3. Print quantity of snack 3.
+        s3.snacksAdd(12);
+        System.out.println("There are now " + s3.getQuantity() + " units of " + s3.getName() + " in the vending machine");
+    
+        //Customer 2 buys 3 of snack 3. Print Customer 2 Cash on hand. Print quantity of snack 3.
+        c2.moneySubtract(s3.getCost() * 3);
+        s3.snacksReduce(3);
+        System.out.println(c2.getName() + " has " + "$" + c2.getCash() + " left. " + "There are " + s3.getQuantity() + " " + s3.getName() + "s left.");
+   
+        System.out.println("********************************************************************************");
+        System.out.println("********************************************************************************");
     }
 
     public static void main(String[] args) {
